fb927609be0df61cbc736335014ea2da1672ff58
chaeelin <chaeee0123@gmail.com>
2025-08-19 19:10:15 +0900

    :fire: Remove: QuantityReqeust 함수 제거

--- models/logic_model.py
+++ models/logic_model.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
-from typing import Union
 
 # 주문 시작 요청
 class StartOrderRequest(BaseModel):
@@ -15,17 +14,6 @@ class MenuRequest(BaseModel):
                 "menu_item": "라면"
             }
         }
-
-class QuantityRequest(BaseModel):
-    quantity: Union[int, str] = Field(..., description="수량 (숫자 또는 자연어)")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "quantity": "2개"
-            }
-        }
-
 
 class PackagingRequest(BaseModel):
     packaging_type: str = Field(..., description="포장 방식")
